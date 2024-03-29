53e8ab4355d7d5f215f7bf790e76409c5144940b
E. Hannah <echannah631@gmail.com>
2024-03-29 08:38:01 -0500

    tests that simulate user events

--- src/Checklist/Checklist.test.tsx
+++ src/Checklist/Checklist.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Checklist } from './Checklist';
+import { IdValue } from './types';
 
 test('should render correct list items when data specified', function () {
   render(
@@ -44,4 +46,39 @@ test('should render correct checked items when specified', function () {
     />
   );
   expect(screen.getByTestId('Checklist__input__1')).toBeChecked();
+});
+
+test('should check items when clicked', async function () {
+  const user = userEvent.setup();
+  render(
+    <Checklist
+      data={[{ id: 1, name: 'Lucy', role: 'Manager' }]}
+      id='id'
+      primary='name'
+      secondary='role'
+    />
+  );
+  const lucyCheckbox = screen.getByTestId('Checklist__input__1');
+  expect(lucyCheckbox).not.toBeChecked();
+  await user.click(lucyCheckbox);
+  expect(lucyCheckbox).toBeChecked();
+});
+
+test('should call onCheckedIdsChange when clicked', async function () {
+  const user = userEvent.setup();
+  let calledWith: IdValue[] | undefined = undefined;
+
+  render(
+    <Checklist
+      data={[{ id: 1, name: 'Lucy', role: 'Manager' }]}
+      id='id'
+      primary='name'
+      secondary='role'
+      onCheckedIdsChange={function (checkedIds) {
+        calledWith = checkedIds;
+      }}
+    />
+  );
+  await user.click(screen.getByTestId('Checklist__input__1'));
+  expect(calledWith).toStrictEqual([1]);
 });
